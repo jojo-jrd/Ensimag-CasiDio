d87a3598a2705eb8680f73c85667959a58b4f19e
daviruss1969 <lukasloiodic@gmail.com>
2024-04-25 08:55:26 +0200

    [FEAT]backend : add tests user

--- backend/src/__tests__/api.user.test.js
+++ backend/src/__tests__/api.user.test.js
@@ -11,7 +11,7 @@ test('Acount creation : simple valid', async () => {
   expect(response.statusCode).toBe(200)
 })
 
-test('Acount creation : not specify a mandatory fields', async () => {
+test('Acount creation : missing mandatory fields', async () => {
   const response = await request(app)
     .post('/register')
     .send({ firstName: 'Test', email: 'test@test.com', password: '1m02P@SsF0rt!', address: '8 rue de gaulle', birthDate: new Date('2002-09-17') })
@@ -26,6 +26,51 @@ test('Acount creation : weak password', async () => {
   
   expect(response.statusCode).toBe(400)
 })
+
+test('Acount creation : bad email', async () => {
+  const response = await request(app)
+    .post('/register')
+    .send({ firstName: 'Test', lastName: 'TEST', email: 'notanemail', password: '1m02P@SsF0rt!', address: '8 rue de gaulle', birthDate: new Date('2002-09-17') })
+  
+  expect(response.statusCode).toBe(400)
+})
+
+// --------------------------------------- //
+//            ACOUNT CONNECTION            //
+// --------------------------------------- //
+test('Acount connection : simple connection', async () => {
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'ab*123456' })
+
+  expect(response.statusCode).toBe(200)
+  expect(response.body).toHaveProperty('token')
+})
+
+test('Acount connection : missing mandatory fields', async () => {
+  let response = await request(app)
+    .post('/login')
+    .send({ password: 'ab*123456' })
+
+  expect(response.statusCode).toBe(400)
+})
+
+test('Acount connection : wrong email', async () => {
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'email@unknown.com', password: 'ab*123456' })
+
+  expect(response.statusCode).toBe(403)
+})
+
+test('Acount connection : wrong password', async () => {
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'ab*123456invalid' })
+
+  expect(response.statusCode).toBe(403)
+})
+
 // test('Test if user can log in and list users', async () => {
 //   let response = await request(app)
 //     .post('/login')
