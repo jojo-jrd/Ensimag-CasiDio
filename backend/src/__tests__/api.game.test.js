3f4d9875a79c1d4c62cb1bd45e938508ed41a7ee
loiodilu <lukas.loiodice@grenoble-inp.org>
2024-05-03 12:10:36 +0200

    [FEAT]backend: add games tests

--- backend/src/__tests__/api.game.test.js
+++ backend/src/__tests__/api.game.test.js
@@ -57,3 +57,132 @@ test('Game creation : simple creation', async () => {
       "description": "Bon jeu"
     })
 })
+
+test('Game creation : missing fields', async () => {
+  // Connection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  // Create a game
+  response = await request(app)
+    .post('/api/game')
+    .set('x-access-token', userToken)
+    .send({ name: 'Poker', page: 'Poker', description: 'Bon jeu'})
+  expect(response.statusCode).toBe(400)
+})
+
+// --------------------------------------- //
+//             GAME EDITION                //
+// --------------------------------------- //
+test('Game edition : simple edit', async () => {
+  // Conection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  // Edit third game
+  response = await request(app)
+    .put('/api/game/3')
+    .set('x-access-token', userToken)
+    .send({ name: 'PokerEdit', picturePath: './images/slot-machine.pngEdited', page: 'PokerEdit', description: 'Bon jeu edit'})
+  expect(response.statusCode).toBe(200)
+
+  // Verify update
+  response = await request(app)
+    .get('/games')
+  expect(response.statusCode).toBe(200)
+  expect(response.body.data.find((game) => game.id === 3)).toStrictEqual({
+    "id": 3,
+    "name": "PokerEdit",
+    "picturePath": "./images/slot-machine.pngEdited",
+    "page": "PokerEdit",
+    "description": "Bon jeu edit"
+  })
+})
+
+test('Game edition : edit one field', async () => {
+  // Conection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  // Edit third game
+  response = await request(app)
+    .put('/api/game/3')
+    .set('x-access-token', userToken)
+    .send({ name: 'PokerEditEdit' })
+  expect(response.statusCode).toBe(200)
+
+  // Verify update
+  response = await request(app)
+    .get('/games')
+  expect(response.statusCode).toBe(200)
+  expect(response.body.data.find((game) => game.id === 3)).toStrictEqual({
+    "id": 3,
+    "name": "PokerEditEdit",
+    "picturePath": "./images/slot-machine.pngEdited",
+    "page": "PokerEdit",
+    "description": "Bon jeu edit"
+  })
+})
+
+test('Game edition : no game found', async () => {
+  // Conection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  response = await request(app)
+    .put('/api/game/10')
+    .set('x-access-token', userToken)
+    .send({ name: 'PokerEditEdit' })
+  expect(response.statusCode).toBe(404)
+})
+
+// --------------------------------------- //
+//              GAME DELETE                //
+// --------------------------------------- //
+test('Game delete : simple deletion', async () => {
+  // Conection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  // Remove third game
+  response = await request(app)
+    .delete('/api/game/3')
+    .set('x-access-token', userToken)
+  expect(response.statusCode).toBe(200)
+
+  // Check that the old game doesn't exist anymore
+  response = await request(app)
+    .get('/games')
+  expect(response.statusCode).toBe(200)
+  expect(response.body.data.map((game) => game.id)).not.toContain(3)
+})
+
+test('Game delete : no game found', async () => {
+  // Conection on admin
+  let response = await request(app)
+    .post('/login')
+    .send({ email: 'a@a.com', password: 'Ab*123-!' })
+  expect(response.statusCode).toBe(200)
+  const userToken = response.body.token
+
+  // Remove third game
+  response = await request(app)
+    .delete('/api/game/10')
+    .set('x-access-token', userToken)
+  expect(response.statusCode).toBe(404)
+})
