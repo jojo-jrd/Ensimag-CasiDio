2c359d2cecc41da9f600a577ff72a072cf66e3a1
Jordan Josserand <jordan.josserand@grenoble-inp.org>
2024-05-08 10:36:43 +0200

    [FEAT] tri sur le tableau des utilisateurs + suppression d'un utilisateur

--- frontend/src/pages/admin_users/AdminUsers.jsx
+++ frontend/src/pages/admin_users/AdminUsers.jsx
@@ -3,86 +3,175 @@ import { AppContext } from '../../AppContext';
 import moment from 'moment';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons/faPenToSquare';
 import { faTrashCan } from '@fortawesome/free-regular-svg-icons/faTrashCan';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import swal from 'sweetalert';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+function DisplaySort({currentSort, column}) {
+    return (
+        <>
+            {column == currentSort.column ? (
+                currentSort.order == 'ASC' ? 
+                    <FontAwesomeIcon className="ml-3" icon={faCaretDown}/>
+                : 
+                    <FontAwesomeIcon className="ml-3" icon={faCaretUp}/>
+            ) : ''}
+        </>
+    )
+}
 
 
 function AdminUsers() {
     const { token } = useContext(AppContext);
+    const [currentSort, setCurrentSort] = useState({
+        column : '',
+        order : 'ASC',
+    });
     const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        async function loadUsers() {
-            const reponse = await (await fetch(`${import.meta.env.VITE_API_URL}/api/users `, {
-                method: 'GET',
-                headers: {
-                    'x-access-token': token,
-                }
-            })).json();
-            if (reponse?.['data']) {
-                setUsers(reponse['data']);
-            } else {
-                console.error(reponse.message);
+    async function loadUsers() {
+        const reponse = await (await fetch(`${import.meta.env.VITE_API_URL}/api/users `, {
+            method: 'GET',
+            headers: {
+                'x-access-token': token,
             }
+        })).json();
+        if (reponse?.['data']) {
+            setUsers(reponse['data']);
+        } else {
+            console.error(reponse.message);
         }
+    }
+
+    useEffect(() => {
         loadUsers();
-    }, [])
+    }, []);
+
+    // TODO: faire update et tester la suppression quand le bug sera résolu
+
 
     function sortColumn(key, type) {
+        let order = currentSort.order;
+        // Si on avait déjà un tri sur cette colonne, on la tri dans l'autre sens
+        if (key == currentSort.column) {
+            order = order == 'DESC' ? 'ASC' : 'DESC';
+        } else {
+            order = 'ASC';
+        }
         const newUsers = [...users];
-        newUsers.sort((a, b) => {
+        const functionSort = (a, b) => {
             if (type == 'string') {
-                return a[key]?.localeCompare(b[key]);
+                const value = a[key]?.localeCompare(b[key])
+                return order == 'ASC' ? value : 0 - value;
             } else { // date
                 let date1 = new Date(a[key]).getTime();
                 let date2 = new Date(b[key]).getTime();
-
+                let value = 0;
                 if (date1 < date2) {
-                    return -1;
+                    value = -1;
                 } else if (date1 > date2) {
-                    return 1
-                } else {
-                    return 0;
+                    value = 1
                 }
+                return order == 'ASC' ? value : 0 - value;
             }
+        }
+        newUsers.sort(functionSort);
+        setCurrentSort({
+            column : key,
+            order
         });
         setUsers(newUsers);
     }
 
-    return (
-        <div>
-            <table className="w-full mt-4 text-sm text-left rtl:text-right text-gray-500">
-                <thead className="text-xs text-gray-800 uppercase bg-gray-200">
-                    <tr>
-                        {/* TODO: affichage du sort */}
-                        <th className="px-6 py-3" onClick={() => sortColumn('firstName', 'string')}>Prenom</th>
-                        <th className="px-6 py-3" onClick={() => sortColumn('lastName', 'string')}>Nom</th>
-                        <th className="px-6 py-3" onClick={() => sortColumn('email', 'string')}>Email</th>
-                        <th className="px-6 py-3" onClick={() => sortColumn('address', 'string')}>Adresse</th>
-                        <th className="px-6 py-3" onClick={() => sortColumn('birthDate', 'date')}>Date de naissance</th>
-                        <th className="px-6 py-3"></th>
-                        <th className="px-6 py-3"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {users.map(user => {
-                        return (
-                            <tr key={user.id} className="odd:bg-white even:bg-gray-50 border-b">
-                                <td className="px-6 py-3">{user.firstName}</td>
-                                <td className="px-6 py-3">{user.lastName}</td>
-                                <td className="px-6 py-3">{user.email}</td>
-                                <td className="px-6 py-3">{user.address}</td>
-                                <td className="px-6 py-3">{moment(user.birthDate).format('L')}</td>
-                                <td className="px-6 py-3"><FontAwesomeIcon className="ml-3 text-blue-700 cursor-pointer" icon={faPenToSquare} /></td>
-                                <td className="px-6 py-3"><FontAwesomeIcon className="ml-3 text-red-500 cursor-pointer" icon={faTrashCan} /></td>
-                            </tr>
-                        )
-                    })
-
+    function deleteUser(user) {
+        swal({
+            title: "Attention",
+            text: `Êtes-vous sûr de supprimer l'utilisateur ${user.firstName} ${user.lastName} ?`,
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then(async (willDelete) => {
+            if (willDelete) {
+                // Suppression de l'utilisateur
+                const reponse = await(await fetch(`${import.meta.env.VITE_API_URL}/api/user`, {
+                    method: 'DELETE',
+                    headers: {
+                        'x-access-token': token,
+                    },
+                    body : {
+                        userId : user.id
                     }
-                </tbody>
+                })).json();
+                if (reponse.status) {
+                    toast.success("Utilisateur supprimé", {
+                        position: "top-right",
+                        theme: "colored",
+                      });
+                    loadUsers();
+                } else {
+                    console.error(reponse);
+                }
 
-            </table>
-        </div>
+            }
+          });
+          
+    }
+
+    return (
+        <>
+            <div>
+                <table className="w-full mt-4 text-sm text-left rtl:text-right text-gray-500">
+                    <thead className="text-xs text-gray-800 uppercase bg-gray-200">
+                        <tr>
+                            <th className="px-6 py-3 cursor-pointer" onClick={() => sortColumn('firstName', 'string')}>
+                                Prenom
+                                <DisplaySort column='firstName' currentSort={currentSort}/>
+                            </th>
+                            <th className="px-6 py-3 cursor-pointer" onClick={() => sortColumn('lastName', 'string')}>
+                                Nom
+                                <DisplaySort column='lastName' currentSort={currentSort}/>
+                            </th>
+                            <th className="px-6 py-3 cursor-pointer" onClick={() => sortColumn('email', 'string')}>
+                                Email
+                                <DisplaySort column='email' currentSort={currentSort}/>
+                            </th>
+                            <th className="px-6 py-3 cursor-pointer hidden md:table-cell" onClick={() => sortColumn('address', 'string')}>
+                                Adresse
+                                <DisplaySort column='address' currentSort={currentSort}/>
+                            </th>
+                            <th className="px-6 py-3 cursor-pointer hidden md:table-cell" onClick={() => sortColumn('birthDate', 'date')}>
+                                Date de naissance
+                                <DisplaySort column='birthDate' currentSort={currentSort}/>
+                            </th>
+                            <th className="px-6 py-3"></th>
+                            <th className="px-6 py-3"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => {
+                            return (
+                                <tr key={user.id} className="odd:bg-white even:bg-gray-50 border-b">
+                                    <td className="px-6 py-3">{user.firstName}</td>
+                                    <td className="px-6 py-3">{user.lastName}</td>
+                                    <td className="px-6 py-3">{user.email}</td>
+                                    <td className="px-6 py-3 hidden md:table-cell">{user.address}</td>
+                                    <td className="px-6 py-3 hidden md:table-cell">{moment(user.birthDate).format('L')}</td>
+                                    <td className="px-6 py-3"><FontAwesomeIcon className="ml-3 text-blue-700 cursor-pointer" icon={faPenToSquare}/></td>
+                                    <td className="px-6 py-3"><FontAwesomeIcon className="ml-3 text-red-500 cursor-pointer" icon={faTrashCan} onClick={() => deleteUser(user)}/></td>
+                                </tr>
+                            )
+                        })
+
+                        }
+                    </tbody>
+                </table>
+            </div>
+            <ToastContainer />
+        </>
     )
 }
 
