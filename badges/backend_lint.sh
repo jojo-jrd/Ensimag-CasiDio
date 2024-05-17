b3e4e4c4e5a86b2adaaeba8e98bd1c4099aed9b9
Jordan Josserand <jordan.josserand@grenoble-inp.org>
2024-05-17 14:50:23 +0200

    [FIX] refactoring + fix badge linter

--- badges/backend_lint.sh
+++ badges/backend_lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NBERR=$(grep -e "error" backend_lint_report.txt | wc -l)
-NBWARN=$(grep -e "warning" backend_lint_report.txt | wc -l)
+NBERR=$(grep -e "error" backend_lint_report.txt | wc -l) - 1
+NBWARN=$(grep -e "warning" backend_lint_report.txt | wc -l) - 1
 color="green"
 if [[ $NBERR > 0 ]]
 then 
