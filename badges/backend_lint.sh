509ac1a2c5011e525149efcd01578edfcb37f924
Jordan Josserand <jordan.josserand@grenoble-inp.org>
2024-05-21 10:06:45 +0200

    [FIX] badges lint

--- badges/backend_lint.sh
+++ badges/backend_lint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 NBERR=$(grep -e "error" backend_lint_report.txt | wc -l)
 NBWARN=$(grep -e "warning" backend_lint_report.txt | wc -l)
-NBERR=$(($NBERR - 1))
+
 NBWARN=$(($NBWARN - 1))
+
 color="green"
 if [[ $NBERR > 0 ]]
 then 
+  NBERR=$(($NBERR - 1))
   color="red"
-  else if [[ $NBWARN > 0 ]]
+  else if [[ $NBWARN > 1 ]]
   then 
+    NBWARN=$(($NBWARN - 1))
     color="orange"
   fi
 fi
--- badges/frontend_lint.sh
+++ badges/frontend_lint.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 NBERR=$(grep -e "error" frontend_lint_report.txt | wc -l)
 NBWARN=$(grep -e "warning" frontend_lint_report.txt | wc -l)
-NBERR=$(($NBERR - 1))
+
+# Warning de la commande
 NBWARN=$(($NBWARN - 1))
+
 color="green"
-if [[ $NBERR > 0 ]]
-then 
+if [[ $NBERR > 1 ]]
+then
+  # Error du récapitulatif
+  NBERR=$(($NBERR - 1))
   color="red"
-  else if [[ $NBWARN > 0 ]]
-  then 
+  else if [[ $NBWARN > 1 ]]
+  then
+    # Warning du récapitulatif
+    NBWARN=$(($NBWARN - 1))
     color="orange"
   fi
 fi
